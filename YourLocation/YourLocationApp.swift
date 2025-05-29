5cba27a61086f5ee9ee21474f395f84c06921950
PiotrK <piotr.koscielny69@gmail.com>
2025-05-29 10:06:25 +0200

    Initial Commit

---
+++ YourLocation/YourLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourLocationApp.swift
+//  YourLocation
+//
+//  Created by piotr koscielny on 29/5/25.
+//
+
+import SwiftUI
+
+@main
+struct YourLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
